4c1e20b0c5c164a5def8eeb7e22cfbcfe04409de
michalZ67214 <w67214@student.wsiz.edu.pl>
2023-11-07 11:49:47 +0100

    lab2 - zadanie 1

---
+++ lab2/Licz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Licz
+    {
+        private double value;
+
+        public Licz(double value)
+        {
+            this.value = value;
+        }
+
+        private double Dodaj(double a)
+        {
+            return this.value += a;
+        }
+
+        private double Odejmij(double a)
+        {
+            return this.value -= a;
+        }
+
+        public void View(double a)
+        {
+            Console.WriteLine($"Wartosc poczatkowa: {this.value},\nWartosc po dodaniu {a}: " +
+                $"{Dodaj(a)}, \nWartosc po odjeciu {a}: {this.Odejmij(a)}\n");
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Zadanie 1
+            Licz[] liczs = new Licz[]
+            {
+                new Licz(1),
+                new Licz(2),
+                new Licz(3)
+            };
+
+            foreach(Licz item in liczs)
+            {
+                item.View(2);
+                Console.WriteLine(item.ToString());
+                Console.WriteLine();
+            }
+
+            // Zadanie 1 koniec
+        }
+    }
+}
